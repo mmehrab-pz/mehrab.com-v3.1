625004201e3f5fb32b9d53b50d9f847665ff0219
mehrab poorzakaria <99506317+mmehrab-pz@users.noreply.github.com>
2023-07-23 22:34:11 +0330

    Add files via upload

--- asset/js/master.js
+++ asset/js/master.js
@@ -1,12 +1,28 @@
-function open_contact(){
-    document.getElementById('contact').style.transform = 'translateX(0)'
+// ---------------------------contact------------------------
+function open_contact() {
     document.getElementById('contact').style.visibility = 'visible'
     document.getElementById('contact').style.opacity = '1'
-    document.getElementById('contact').style.display = 'flex'
+    document.getElementById('contact').style.transform = 'scaleX(1)'
+    document.getElementById('contact').style.left = '0'
+
 }
-function close_contact(){
-    document.getElementById('contact').style.transform = 'translateX(100%)'
+
+function close_contact() {
     document.getElementById('contact').style.visibility = 'hidden'
     document.getElementById('contact').style.opacity = '1'
-    document.getElementById('contact').style.display = 'none'
+    document.getElementById('contact').style.transform = 'scaleX(0)'
+    document.getElementById('contact').style.left = '100%'
+}
+// ---------------------------strength------------------------
+function open_strength() {
+    document.getElementById('strength').style.opacity = '1'
+    document.getElementById('strength').style.transform = 'scaleY(1)'
+    document.getElementById('strength').style.top = '0'
+
+}
+
+function close_strength() {
+    document.getElementById('strength').style.opacity = '0'
+    document.getElementById('strength').style.transform = 'scaleY(0)'
+    document.getElementById('strength').style.top = '100%'
 }
